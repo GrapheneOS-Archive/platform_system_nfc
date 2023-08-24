4f76b59526042997a0c036ca04cd36e880963d1a
Henri Chataing <henrichataing@google.com>
2023-08-24 15:37:43 -0700

    Casimir: replace use of select! with joins
    select! has the drawback that alternative branches
    are cancelled when one completes, counter-intuitively.
    
    Bug: 292164611
    Test: m casimir
    Change-Id: Ibd40fd9fcb58e90be9f30cc7df1545c1709dddaf

--- tools/casimir/src/main.rs
+++ tools/casimir/src/main.rs
@@ -27,6 +27,7 @@ use tokio::io::AsyncWriteExt;
 use tokio::net::{tcp, TcpListener, TcpStream};
 use tokio::select;
 use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 
 pub mod controller;
 pub mod packets;
@@ -40,25 +41,26 @@ type Id = usize;
 /// Read NCI Control and Data packets received on the NCI transport.
 /// Performs recombination of the segmented packets.
 pub struct NciReader {
-    socket: tcp::OwnedReadHalf,
+    socket: Mutex<tcp::OwnedReadHalf>,
 }
 
 /// Write NCI Control and Data packets received to the NCI transport.
 /// Performs segmentation of the packets.
 pub struct NciWriter {
-    socket: tcp::OwnedWriteHalf,
+    socket: Mutex<tcp::OwnedWriteHalf>,
 }
 
 impl NciReader {
     /// Create a new NCI reader from the TCP socket half.
     pub fn new(socket: tcp::OwnedReadHalf) -> Self {
-        NciReader { socket }
+        NciReader { socket: Mutex::new(socket) }
     }
 
     /// Read a single NCI packet from the reader. The packet is automatically
     /// re-assembled if segmented on the NCI transport.
-    pub async fn read(&mut self) -> Result<Vec<u8>> {
+    pub async fn read(&self) -> Result<Vec<u8>> {
         const HEADER_SIZE: usize = 3;
+        let mut socket = self.socket.lock().await;
         let mut complete_packet = vec![0; HEADER_SIZE];
 
         // Note on reassembly:
@@ -70,13 +72,13 @@ impl NciReader {
         // packet.
         loop {
             // Read the common packet header.
-            self.socket.read_exact(&mut complete_packet[0..HEADER_SIZE]).await?;
+            socket.read_exact(&mut complete_packet[0..HEADER_SIZE]).await?;
             let header = nci::PacketHeader::parse(&complete_packet[0..HEADER_SIZE])?;
 
             // Read the packet payload.
             let payload_length = header.get_payload_length() as usize;
             let mut payload_bytes = vec![0; payload_length];
-            self.socket.read_exact(&mut payload_bytes).await?;
+            socket.read_exact(&mut payload_bytes).await?;
             complete_packet.extend(payload_bytes);
 
             // Check the Packet Boundary Flag.
@@ -91,12 +93,13 @@ impl NciReader {
 impl NciWriter {
     /// Create a new NCI writer from the TCP socket half.
     pub fn new(socket: tcp::OwnedWriteHalf) -> Self {
-        NciWriter { socket }
+        NciWriter { socket: Mutex::new(socket) }
     }
 
     /// Write a single NCI packet to the writer. The packet is automatically
     /// segmented if the payload exceeds the maximum size limit.
-    async fn write(&mut self, mut packet: &[u8]) -> Result<()> {
+    async fn write(&self, mut packet: &[u8]) -> Result<()> {
+        let mut socket = self.socket.lock().await;
         let mut header_bytes = [packet[0], packet[1], 0];
         packet = &packet[3..];
 
@@ -114,8 +117,8 @@ impl NciWriter {
             header_bytes[2] = chunk_length as u8;
 
             // Write the header and payload segment bytes.
-            self.socket.write_all(&header_bytes).await?;
-            self.socket.write_all(&packet[..chunk_length]).await?;
+            socket.write_all(&header_bytes).await?;
+            socket.write_all(&packet[..chunk_length]).await?;
             packet = &packet[chunk_length..];
 
             if packet.is_empty() {
@@ -142,14 +145,14 @@ impl Device {
             rf_tx,
             task: Box::pin(async move {
                 let (nci_rx, nci_tx) = socket.into_split();
-                let mut controller = Controller::new(
+                Controller::run(
                     id,
                     NciReader::new(nci_rx),
                     NciWriter::new(nci_tx),
                     rf_rx,
                     controller_rf_tx,
-                );
-                controller.run().await
+                )
+                .await
             }),
         }
     }
